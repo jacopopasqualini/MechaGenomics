c219be2dc0e0d9714e247df23950a166faf3d6f1
jacopo.pasqualini <jacopo.pasqualini@yahoo.it>
2022-09-23 15:08:16 +0000

    Renamed snakemake file for PE shotgun reads and drafted db_scraper

--- resources/db_scraper.py
+++ resources/db_scraper.py
@@ -12,6 +12,19 @@ for D in RES_DIR:
     if not os.path.exists(D):
 
         subprocess.run(['mkdir',D])
-        
+
+# REFSEQ KAIJU GETTER
+# https://kaiju.binf.ku.dk/server
+         
+kaijuRefSeq_db_page = 'https://kaiju.binf.ku.dk/database/kaiju_db_refseq_2022-03-23.tgz'
+
+kaijuRefSeq_files=['kaiju_db_refseq.fmi', 'names.dmp', 'nodes.dmp']
+
+if os.listdir('kaijuRefSeq')!=kaijuRefSeq_files:
+
+    subprocess.run(['wget','https://kaiju.binf.ku.dk/database/kaiju_db_refseq_2022-03-23.tgz'])
+
 # CORE KAIJU GETTER
+
+
 
